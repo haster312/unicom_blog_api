6d931f1882fd39eddae40e5c00581c99b4ee998e
Hoang Nguyen <hoanglc1989@gmail.com>
2020-09-21 19:57:03 +1000

    Article list

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
     {
         header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
         header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length');
-        header('Access-Control-Allow-Origin: http://localhost:3000');
+//        header('Access-Control-Allow-Origin: http://localhost:3000');
         header('Access-Control-Allow-Credentials: true');
     }
 
