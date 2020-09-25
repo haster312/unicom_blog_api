596afb0e79a3145cb515d71238094b17eb42edf8
Hoang Nguyen <hoanglc1989@gmail.com>
2020-09-26 00:23:46 +1000

    add cloudfront url

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -9,4 +9,22 @@ class Image extends Model
     use Base;
     protected $table = 'image';
     protected $fillable = ['main', 'thumbnail', 'mime', 'type'];
+
+    public function getMainAttribute($value)
+    {
+        $assetUrl = env('ASSET_URL');
+        $value = explode("uploads", $value);
+        $url = end($value);
+
+        return $assetUrl . "/uploads" . $url;
+    }
+
+    public function getThumbnailAttribute($value)
+    {
+        $assetUrl = env('ASSET_URL');
+        $value = explode("uploads", $value);
+        $url = end($value);
+
+        return $assetUrl . "/uploads" . $url;
+    }
 }
